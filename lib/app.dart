87d6adb83cbec7e20f1219a5b0bde542b4ce5d0a
ru-461 <48552543+ru-461@users.noreply.github.com>
2023-07-23 15:23:02 +0900

    chore: Create application base

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:nekosophy/views/home.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nekosophy',
+      theme: ThemeData(useMaterial3: true, brightness: Brightness.light),
+      darkTheme: ThemeData(useMaterial3: true, brightness: Brightness.dark),
+      themeMode: ThemeMode.system,
+      home: const Home(),
+    );
+  }
+}
