8f16816dd34b2960a8f9414f7c008b0b0a612ac4
ru-461 <48552543+ru-461@users.noreply.github.com>
2023-07-23 16:54:33 +0900

    update: Create fetch image base

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+import 'package:nekosophy/repository/cat_repository.dart';
+import 'package:nekosophy/repository/meigen_repository.dart';
+
+final logger = Logger();
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,8 +13,64 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String catImageUrl = 'https://cdn2.thecatapi.com/images/9sk.jpg';
+  String meigen = '';
+  String auther = '';
+
+  final _catRepository = CatRepository();
+  final _meigenRepository = MeigenRepository();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Image.network(catImageUrl),
+          ),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Text(
+                    meigen,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 10.0),
+                  Text(
+                    'by $auther',
+                    style: const TextStyle(
+                        fontSize: 14.0, fontStyle: FontStyle.italic),
+                  ),
+                ],
+              )),
+          IconButton(
+              onPressed: () async {
+                // Cat
+                final catList = await _catRepository.fetchCats();
+                final catData = catList.first;
+
+                // Meigen
+                final meigenList = await _meigenRepository.fetchMeigens();
+                final meigenData = meigenList.first;
+
+                // 画像
+                setState(() {
+                  // 画像を更新
+                  catImageUrl = catData.url;
+                  // 名言を更新
+                  meigen = meigenData.meigen;
+                  auther = meigenData.auther;
+                });
+              },
+              icon: const Icon(Icons.refresh))
+        ],
+      ),
+    ));
   }
 }
