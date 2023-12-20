2603aaa0c781d11a9d09370c7609e2b5e53e854c
ru-461 <48552543+ru-461@users.noreply.github.com>
2023-12-20 21:32:40 +0900

    update: Update provider

--- lib/providers/cats_future_provider.g.dart
+++ lib/providers/cats_future_provider.g.dart
@@ -6,22 +6,19 @@ part of 'cats_future_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$catsFutureProviderHash() =>
-    r'5863572cf42abc46f0178241a45472bc354dd714';
+String _$catsFutureHash() => r'757821905e777a38676f07ecf22abd1aabb2edfc';
 
-/// See also [catsFutureProvider].
-@ProviderFor(catsFutureProvider)
-final catsFutureProviderProvider =
-    AutoDisposeFutureProvider<List<Cat>>.internal(
-  catsFutureProvider,
-  name: r'catsFutureProviderProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$catsFutureProviderHash,
+/// See also [catsFuture].
+@ProviderFor(catsFuture)
+final catsFutureProvider = AutoDisposeFutureProvider<List<Cat>>.internal(
+  catsFuture,
+  name: r'catsFutureProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$catsFutureHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef CatsFutureProviderRef = AutoDisposeFutureProviderRef<List<Cat>>;
+typedef CatsFutureRef = AutoDisposeFutureProviderRef<List<Cat>>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
