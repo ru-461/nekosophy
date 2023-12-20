f68a6d2d268653c6a5f57a8a967ac44c80fcd716
ru-461 <48552543+ru-461@users.noreply.github.com>
2023-12-20 22:57:29 +0900

    update: Create future provider

---
+++ lib/providers/future_privider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'future_privider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$futureHash() => r'dc6a41372ecf8ccda6d15ff3cd4cb29584fee0a1';
+
+/// See also [future].
+@ProviderFor(future)
+final futureProvider = AutoDisposeFutureProvider<List>.internal(
+  future,
+  name: r'futureProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$futureHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FutureRef = AutoDisposeFutureProviderRef<List>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
